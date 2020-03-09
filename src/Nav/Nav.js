738e8730e0f569b6a3205d3d4971556ea9a6cef4
Safarov <elkhan.safarov10@yandex.com>
2020-03-09 23:13:03 +0500

    Верстка компонентов

---
+++ src/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './Nav.css';
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className="header-nav">
+          <div className="container">
+          <nav>
+              <ul>
+                <li><a href="#">Главная</a></li>
+                <li><a href="#">Пункты обмена</a></li>
+                <li><a href="#">Контакты</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Nav;
